1710df8442a6f95740ad89a3cd27e89b3a253dff
Fabian Heusser <fabian.heusser@gmail.com>
2014-10-17 06:25:56 +0200

    corrections

--- DefaultInstWin7.ps1
+++ DefaultInstWin7.ps1
@@ -1,12 +1,11 @@
 #http://trycatchfail.com/blog/post/Painless-Workstation-Setup-with-Boxstarter.aspx
 
 #install ie11 first, else it won't work because an missing update
-<#
+
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 
 Enable-RemoteDesktop
 Install-WindowsUpdate -acceptEula
-#>
 
 cinst DotNet4.5
 cinst DotNet3.5
